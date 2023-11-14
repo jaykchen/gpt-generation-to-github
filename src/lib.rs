6a6c6a31239cd3060f7368cdb1bf59476dd6174f
jaykchen@gmail.com <jaykchen@gmail.com>
2023-11-13 20:22:08 -0500

    get_weather, get_time_of_day, scraper

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,24 @@
 use async_openai_wasi::{
-    types::{ChatCompletionToolArgs, ChatCompletionToolType,
+    types::{
         ChatCompletionFunctionsArgs, ChatCompletionRequestFunctionMessageArgs,
         ChatCompletionRequestSystemMessageArgs, ChatCompletionRequestUserMessageArgs,
-        CreateChatCompletionRequestArgs, FinishReason, Role,
+        ChatCompletionToolArgs, ChatCompletionToolType, CreateChatCompletionRequestArgs,
+        FinishReason, Role,
     },
     Client,
 };
 use chrono::prelude::*;
 use dotenv::dotenv;
+use http_req::{
+    request::{Method, Request},
+    uri::Uri,
+};
+use serde::Deserialize;
 use serde_json::json;
 use slack_flows::{listen_to_channel, send_message_to_channel};
 use std::collections::HashMap;
 use std::env;
+use web_scraper_flows::get_page_text;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -64,17 +71,17 @@ pub async fn run_gpt(
             .r#type(ChatCompletionToolType::Function)
             .function(
                 ChatCompletionFunctionsArgs::default()
-                    .name("helloWorld")
+                    .name("getWeather")
                     .description("Prints hello world with the string passed to it")
                     .parameters(json!({
                         "type": "object",
                         "properties": {
-                            "appendString": {
+                            "city": {
                                 "type": "string",
                                 "description": "The string to append to the hello world message",
                             },
                         },
-                        "required": ["appendString"],
+                        "required": ["city"],
                     }))
                     .build()?,
             )
@@ -84,18 +91,16 @@ pub async fn run_gpt(
             .function(
                 ChatCompletionFunctionsArgs::default()
                     .name("scraper")
-                    .description(
-                        "Scraps the book website goodreads for books with the keyword passed to it",
-                    )
+                    .description("Get the text content of the webpage from the url passed to it")
                     .parameters(json!({
                         "type": "object",
                         "properties": {
-                            "keyword": {
+                            "url": {
                                 "type": "string",
-                                "description": "The keyword to search for",
+                                "description": "The url from which to fetch the content",
                             },
                         },
-                        "required": ["keyword"],
+                        "required": ["url"],
                     }))
                     .build()?,
             )
@@ -137,15 +142,15 @@ pub async fn run_gpt(
         for tool_call in tool_calls {
             let function = &tool_call.function;
             let content = match function.name.as_str() {
-                "helloWorld" => {
+                "getWeather" => {
                     let argument_obj =
                         serde_json::from_str::<HashMap<String, String>>(&function.arguments)?;
-                    hello_world(argument_obj["appendString"].clone())
+                    get_weather(&argument_obj["city"].clone())
                 }
                 "scraper" => {
                     let argument_obj =
                         serde_json::from_str::<HashMap<String, String>>(&function.arguments)?;
-                    scraper(argument_obj["keyword"].clone()).await?
+                    scraper(argument_obj["url"].clone()).await
                 }
                 "getTimeOfDay" => get_time_of_day(),
                 _ => "".to_string(),
@@ -184,15 +189,89 @@ pub async fn run_gpt(
     Ok(())
 }
 
-fn hello_world(append_string: String) -> String {
-    format!("Hello, world! {}", append_string)
+fn get_weather(city: &str) -> String {
+    if let Some(w) = get_weather_inner(&city) {
+        format!(
+            r#"
+Today in {}
+{}
+Low temperature: {} °C,
+High temperature: {} °C,
+Wind Speed: {} km/h"#,
+            city,
+            w.weather
+                .first()
+                .unwrap_or(&Weather {
+                    main: "Unknown".to_string()
+                })
+                .main,
+            w.main.temp_min as i32,
+            w.main.temp_max as i32,
+            w.wind.speed as i32
+        )
+    } else {
+        String::from("No city or incorrect spelling")
+    }
 }
 
-async fn scraper(keyword: String) -> Result<String, Box<dyn std::error::Error>> {
-    Ok(format!("Scraped books with keyword: {}", keyword))
+async fn scraper(url: String) -> String {
+    match get_page_text(&url).await {
+        Err(_e) => "failed to get webpage".to_string(),
+
+        Ok(txt) => txt,
+    }
 }
 
 fn get_time_of_day() -> String {
     let now = Local::now();
     now.to_rfc3339()
+}
+
+#[derive(Deserialize, Debug)]
+struct ApiResult {
+    weather: Vec<Weather>,
+    main: Main,
+    wind: Wind,
+}
+
+#[derive(Deserialize, Debug)]
+struct Weather {
+    main: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Main {
+    temp_max: f64,
+    temp_min: f64,
+}
+
+#[derive(Deserialize, Debug)]
+struct Wind {
+    speed: f64,
+}
+
+fn get_weather_inner(city: &str) -> Option<ApiResult> {
+    let mut writer = Vec::new();
+    let api_key = env::var("API_KEY").unwrap_or("fake_api_key".to_string());
+    let query_str = format!(
+        "https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
+    );
+
+    let uri = Uri::try_from(query_str.as_str()).unwrap();
+    match Request::new(&uri).method(Method::GET).send(&mut writer) {
+        Err(_e) => {}
+
+        Ok(res) => {
+            if !res.status_code().is_success() {
+                return None;
+            }
+            match serde_json::from_slice::<ApiResult>(&writer) {
+                Err(_e) => {}
+                Ok(w) => {
+                    return Some(w);
+                }
+            }
+        }
+    };
+    None
 }
